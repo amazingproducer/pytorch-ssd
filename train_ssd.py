eba12248bdf0883114febde9ef31a8f57a5a7f45
amazingproducer <amazingproducer@gmail.com>
2021-10-13 00:50:18 -1000

    doot

--- train_ssd.py
+++ train_ssd.py
@@ -353,9 +353,8 @@ if __name__ == '__main__':
                     opt_resume_path = os.path.join(o_dir, n)
 #        optimizer.load_state_dict(torch.load(opt_resume_path)['state_dict'])
         optimizer.load_state_dict(torch.load(opt_resume_path))
-        for i in optimizer.state_dict():
-            for j in i.keys():
-                print(f"{i}: {i[j]}")
+        for i in optimizer.state_dict()['state']:
+            print(i)
     logging.info(f"Learning rate: {args.lr}, Base net learning rate: {base_net_lr}, "
                  + f"Extra Layers learning rate: {extra_layers_lr}.")
 
